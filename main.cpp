46bc12ea531178d69a408e110ed03b9b9a07266f
Pedro da Fonseca <36480154+Drosaca@users.noreply.github.com>
2022-07-20 00:58:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,140 +1,34 @@
-#include "wx/wxprec.h"
-#include <wx/rawbmp.h>
-#include "Camera.h"
-#include "camera.h"
+#include "raylib.h"
+#include <stdlib.h>
 
-#ifdef __BORLANDC__
-    #pragma hdrstop
-#endif
-
-// for all others, include the necessary headers (this file is usually all you
-// need because it includes almost all "standard" wxWidgets headers)
-#ifndef WX_PRECOMP
-    #include "wx/wx.h"
-#endif
-
-class MyFrame: public wxFrame
+int main(void)
 {
-    public:
-        MyFrame();
+    InitWindow(800, 450, "raylib [core] example - basic window");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
-    private:
-        void OnPaint(wxPaintEvent& event);
+    while (!WindowShouldClose())
+    {
+            int h = 1000;
+            int w = 1000;
 
-        wxPanel* m_board;
-};
-
-wxBitmap *gImg = NULL;
-bool gImgLock = false;
-
-MyFrame::MyFrame()
-        :wxFrame(NULL, wxID_ANY, "Chess", wxDefaultPosition, wxSize(1000, 1000))
-{
-    m_board = new wxPanel(this, wxID_ANY);
-    m_board->Bind(wxEVT_PAINT, &MyFrame::OnPaint, this);
-
-
-}
-
-
-
-void LoadImg(Frame_t *frame) {
-    
-   /* gImg = new wxBitmap(frame->width, frame->height, 24);
-    std::cout << "load" << frame->width << std::endl;
-    if(frame->imageSize == 0)
-        return;
-    wxNativePixelData data(*gImg);
-    wxNativePixelData::Iterator p(data);
-    p.Offset(data, 0, 0);
-
-    for(int y =0; y < frame->height; y++) {
-        wxNativePixelData::Iterator rowStart = p;
-        for(int x = 0; x < frame->width; x++) {
-            U32 index1d = frame->width * y + x; 
-            U16 pixel = ((U16 *)&frame->pixels[0])[index1d];
-            double intensity = pixel / pow(2, GET_PIXEL_SIZE(frame->pixelFormat));
-            U8 luminance = intensity * 255;
-            p.Red() = luminance;
-            p.Green() = luminance;
-            p.Blue() = luminance; 
-        }
-        p = rowStart;
-        p.OffsetY(data, 1);
-    }*/
-    int h=100;
-    int w = 100;
-    gImg = new wxBitmap(100, 100, 24); 
-if(!bitmap->Ok()) {
-		delete bitmap;
-		return NULL;
-	}
-
-	PixelData bmdata(*bitmap);
-	if(bmdata==NULL) {
-		wxLogDebug(wxT("getBitmap() failed"));
-		delete bitmap;
-		return NULL;
-	}
-
-	PixelData::Iterator dst(bmdata);
-
-	for(int y=0; y<h; y++) {
-		dst.MoveTo(bmdata, 0, y);
-		for(int x=0; x<w; x++) {
-			// wxBitmap contains rgb values pre-multiplied with alpha
-			dst.Red()=255;
-			dst.Green()=0;
-			dst.Blue()=0;
-			dst++;
-		}
-	}
-    
-}
-
-
-void MyFrame::OnPaint(wxPaintEvent&)
-{
-    wxPaintDC dc(m_board);
-    dc.Clear();
-    std::cout << "PAINT.........." << std::endl;
-    if(gImg){
-        dc.DrawBitmap(*gImg, 0, 0, false);
+            ClearBackground(RAYWHITE);
+            Image image = GenImageColor(w, h, BLACK);
+                for(int x = 0; x < h * w; x++) {
+                    int r = rand() % 255;
+                    int g = rand() % 255;
+                    int b = rand() % 255;
+                    ((u_int8_t *)image.data)[x * 4] = r;
+                    ((u_int8_t *)image.data)[x * 4 + 1] = g;
+                    ((u_int8_t *)image.data)[x * 4 + 2] = b;
+                    ((u_int8_t *)image.data)[x * 4 + 3] = 255;
+                }
+        Texture texture = LoadTextureFromImage(image);
+        BeginDrawing();
+        DrawTexture(texture, 0, 0, WHITE);
+        EndDrawing();
     }
-    gImgLock = false;
 
+    CloseWindow();
+
+    return 0;
 }
-
-
-void on_frame(TeaCam_t *self, Frame_t *frame) {
-    printf("%d * %d\n", frame->width, frame->height);
-    printf("expected %d\n", frame->width * frame->height * 2 + 58 + 2);
-    fps_counter();
-    if (!gImgLock) {
-        gImgLock = true;
-        LoadImg(frame);
-    }
-    self->queue(self, frame);
-}
-
-
-class MyApp : public wxApp
-{
-    public:
-    TeaCam_t *camera;
-    void Backend() {
-        camera = new_camera();
-        run_camera(camera);
-        camera->set_callback(camera, on_frame);
-    }
-        virtual bool OnInit()
-        {
-            ::wxInitAllImageHandlers();
-            this->Backend();
-            MyFrame* frame = new MyFrame();
-            frame->Show();
-            return true;
-        }
-};
-
-wxIMPLEMENT_APP(MyApp);
